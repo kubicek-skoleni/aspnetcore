d2f2c4d7e178096e5d2cc84cc6adb2550785efeb
Lukas Kubicek <kubicek.brno@gmail.com>
2022-06-27 13:38:22 +0200

    Statistics controller

---
+++ AspNetCoreSolution/WEBAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        public StatisticsController()
+        {
+            Data.DataSet.LoadFromXML(@"C:\Users\StudentEN\source\repos\kubicek-skoleni\aspnetcore\dataset.xml");
+        }
+
+        [HttpGet("People/All")]
+        public int PeopleCount()
+        {
+            return Data.DataSet.People.Count();
+        }
+
+        [HttpGet("Peple/WithContract")]
+        public int PeopleWithContract()
+        {
+            return Data.DataSet.People
+                        .Where(p => p.Contracts.Count() > 0)
+                        .Count();
+        }
+
+        [HttpGet("/Contracts/Active")]
+        public int ContractsCount()
+        {
+            // pocet vsech aktivnich smluv
+            int cnt = 0;
+
+            foreach(var person in Data.DataSet.People)
+            {
+                cnt += person.Contracts
+                    .Where(c => c.IsActive)
+                    .Count();
+            }
+
+            return cnt;
+        }
+
+    }
+}
