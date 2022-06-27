2673c06c5910c6924c2de79eba20143354df1a50
Vaclav <v.tarantik@gmail.com>
2022-06-27 11:04:25 +0200

    Model - Person, Address, Contract

---
+++ AspNetCoreSolution/Model/Contract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    // * název smlouvy (name)
+    // * evidenční číslo (number)
+    // * datum podpisu (signed)
+    // * zda je platná (active)
+
+    public class Contract
+    {
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+
+        public DateTime Signed { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ AspNetCoreSolution/Model/Person.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class Person
+    {
+        private bool _inicialized;
+
+        #region konstruktory
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person()
+        {
+
+        }
+        #endregion
+
+        #region vlastnosti
+        public string FirstName { get; set; } = "John";
+
+        public string LastName { get; set; } = "Doe";
+
+        public string FullName
+        {
+            get
+            {
+                return GetFullName();
+            }
+        }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Address HomeAddress { get; set; }
+                                        = new Address();
+        [MaxLength(128)]
+        public string Email { get; set; }
+
+        public List<Contract> Contracts { get; set; }
+                                    = new List<Contract>();
+        #endregion
+
+        #region metody
+        public override string ToString()
+            => $"{FirstName} {LastName}";
+
+
+        public int Age()
+        {
+            DateTime today = DateTime.Today;
+
+            int age = today.Year - DateOfBirth.Year;
+
+            if (today.Month < DateOfBirth.Month ||
+           ((today.Month == DateOfBirth.Month) && (today.Day < DateOfBirth.Day)))
+            {
+                age--;  //birthday in current year not yet reached, we are 1 year younger ;)
+            }
+
+            return age;
+        }
+
+        public string GetFullName()
+            => $"{FirstName} {LastName}";
+
+        #endregion
+
+    }
+}
