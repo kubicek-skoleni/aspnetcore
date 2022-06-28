01713b4f28ae8a43d41077cb875cae1a85db8965
Lukas Kubicek <kubicek.brno@gmail.com>
2022-06-28 13:05:30 +0200

    Index nad sloupeckem Email u Person

---
+++ AspNetCoreSolution/Data/Migrations/20220628110456_emailIndex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class emailIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_Email",
+                table: "Persons",
+                column: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Persons_Email",
+                table: "Persons");
+        }
+    }
+}
--- AspNetCoreSolution/Model/Person.cs
+++ AspNetCoreSolution/Model/Person.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Model
 {
+    [Index(nameof(Email))]
     public class Person
     {
      
