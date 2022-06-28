bf9e5cad552666b43f12d7ced44eb243e29abeb8
Lukas Kubicek <kubicek.brno@gmail.com>
2022-06-28 10:44:05 +0200

    Contracts Controller - generovan

---
+++ AspNetCoreSolution/WEBAPI/Controllers/ContractsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Model;
+
+namespace WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractsController : ControllerBase
+    {
+        private readonly PeopleContext _context;
+
+        public ContractsController(PeopleContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contracts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contract>>> GetContracts()
+        {
+            return await _context.Contracts.ToListAsync();
+        }
+
+        // GET: api/Contracts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contract>> GetContract(int id)
+        {
+            var contract = await _context.Contracts.FindAsync(id);
+
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return contract;
+        }
+
+        // PUT: api/Contracts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContract(int id, Contract contract)
+        {
+            if (id != contract.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contract).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContractExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contracts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contract>> PostContract(Contract contract)
+        {
+            _context.Contracts.Add(contract);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContract", new { id = contract.Id }, contract);
+        }
+
+        // DELETE: api/Contracts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContract(int id)
+        {
+            var contract = await _context.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contracts.Remove(contract);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContractExists(int id)
+        {
+            return _context.Contracts.Any(e => e.Id == id);
+        }
+    }
+}
