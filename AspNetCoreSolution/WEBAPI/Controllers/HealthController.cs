10bbcdf7fd54708289e62d0471e74d84958be3ed
Lukas Kubicek <kubicek.brno@gmail.com>
2022-06-28 14:31:57 +0200

    WebPages Status Page

---
+++ AspNetCoreSolution/WEBAPI/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
