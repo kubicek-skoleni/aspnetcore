d96d2eaed3c1ab2e4037821a3fbf1cd5c052001d
Lukas Kubicek <kubicek.brno@gmail.com>
2022-06-29 10:52:20 +0200

    tabulka poslednich 10 lidi

--- AspNetCoreSolution/BlazorApp/Program.cs
+++ AspNetCoreSolution/BlazorApp/Program.cs
@@ -1,3 +1,4 @@
+using Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddDbContext<PeopleContext>();
 
 var app = builder.Build();
 
