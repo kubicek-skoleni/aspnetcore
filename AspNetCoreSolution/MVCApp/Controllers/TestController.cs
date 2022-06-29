6103da1c06958a7a44e0196793f4eea2a4dd6e11
Lukas Kubicek <kubicek.brno@gmail.com>
2022-06-29 09:07:37 +0200

    MVC

---
+++ AspNetCoreSolution/MVCApp/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Emails")]
+        public IActionResult Emails()
+        {
+            List<string> model = new List<string>() 
+            {
+                "example@example.com",
+                "pokus@pokus.cz"
+            };
+
+            ViewData["extra"] = "Stav";
+            return View(model);
+        }
+    }
+}
---
+++ AspNetCoreSolution/MVCApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        //public DbSet<Person> Persons { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
